ae52315650a9a0b9335111968e2a275ad17b674d
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-06-26 11:32:42 -0400

    Cleaning up the RPC function

--- client/src/rpc/rpc.js
+++ client/src/rpc/rpc.js
@@ -1,86 +1,64 @@
-
 const INIT_REQUEST = {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "omit",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    redirect: "follow",
-    referrer: "no-referrer",
-
+  method: "POST",
+  mode: "cors",
+  cache: "no-cache",
+  credentials: "omit",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  redirect: "follow",
+  referrer: "no-referrer"
 };
-
 
 let sessionID;
 
-
-
 function parseSessionID(result) {
-
-    sessionID = result.sessionid;
-    return result;
+  sessionID = result.sessionid;
+  return result;
 }
-
-
 
 export function restoreWalletRPC(params) {
-
-    return callRpc('restore_deterministic_wallet', params)
-        .then(parseSessionID);
+  return callRpc("restore_deterministic_wallet", params).then(parseSessionID);
 }
-
 
 export function getBalanceRPC(params) {
-    return callRpc('get_balance', params)
+  return callRpc("get_balance", params);
 }
 
-
 export function queryViewKeyRPC() {
-
-    return callRpc('query_key', {key_type: "view_key"});
-
+  return callRpc("query_key", { key_type: "view_key" });
 }
 
 export function queryMnemonicKeyRPC() {
-
-    return callRpc('query_key', {key_type: "mnemonic"});
+  return callRpc("query_key", { key_type: "mnemonic" });
 }
 
 export function transferRPC(params) {
-
-    return callRpc('transfer', params);
+  return callRpc("transfer", params);
 }
 
 export function createWalletRPC() {
-
-    return callRpc('create_wallet')
-        .then(parseSessionID)
-
-
+  return callRpc("create_wallet").then(parseSessionID);
 }
 
 function callRpc(method, params) {
+  const objRequest = {
+    id: 0,
+    jsonrpc: "2.0",
+    method: method,
+    params: params
+  };
 
+  if (sessionID) objRequest.sessionID = sessionID;
 
-    const objRequest = {
-        id:0,
-        jsonrpc:"2.0",
-        method:method,
-        params:params
-    };
-
-    if (sessionID)
-        objRequest.sessionID = sessionID;
-
-    return fetch("https://nelliekins.zapto.org:5000/rpc", {...INIT_REQUEST,  body: JSON.stringify(objRequest)} )
-        .then(response => response.json())
-        .then(function(response) {
-            console.log(response);
-            if (response.result.error)
-                throw new Error(response.result.error);
-            return response.result;
-        });
-
+  return fetch("https://nelliekins.zapto.org:5000/rpc", {
+    ...INIT_REQUEST,
+    body: JSON.stringify(objRequest)
+  })
+    .then(response => response.json())
+    .then(function(response) {
+      console.log(response);
+      if (response.result.error) throw new Error(response.result.error);
+      return response.result;
+    });
 }
