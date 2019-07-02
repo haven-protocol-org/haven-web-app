0537a573a1677cd84e47d6ed2fd6edcfdae3f9bf
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-07-02 10:10:56 -0400

    removed unsued code from faq

--- client/src/pages/_public/faq/index.js
+++ client/src/pages/_public/faq/index.js
@@ -8,21 +8,9 @@ import Content from "../../../components/_public/faq";
 import Footer from "../../../components/footer";
 
 class Faq extends Component {
-  constructor(props) {
-    super(props);
-    this.myRef = React.createRef();
-  }
-
   componentDidMount() {
     window.scrollTo(0, 0);
   }
-
-  handleClick = () => {
-    window.scrollTo({
-      top: 700,
-      behavior: "smooth"
-    });
-  };
 
   render() {
     return (
