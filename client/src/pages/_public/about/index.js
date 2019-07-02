7a92ae7000232298b10dea6f60155ca6541f6eea
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-07-02 10:10:33 -0400

    added about page stub

---
+++ client/src/pages/_public/about/index.js
@@ -0,0 +1,31 @@
+// Library Imports
+import React, { Component } from "react";
+
+// Relative Imports
+import { Container, Microcopy, Heading, Page } from "./styles";
+
+import Content from "../../../components/_public/faq";
+import Footer from "../../../components/footer";
+
+class About extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return (
+      <Page>
+        <Container>
+          <Microcopy>
+            <Heading>Haven Protocol</Heading>
+            <Heading>Team</Heading>
+          </Microcopy>
+        </Container>
+        <Content />
+        <Footer />
+      </Page>
+    );
+  }
+}
+
+export default About;
