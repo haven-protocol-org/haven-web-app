d26b392493d1202dc5650f16b4854176bd55e143
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-07-17 13:59:37 -0500

    Stubbing load mroe component

---
+++ client/src/components/load-more/index.js
@@ -0,0 +1,13 @@
+// Library Imports
+import React, { Component } from "react";
+
+// Relative Imports
+import { Container } from "./styles";
+
+class LoadMore extends Component {
+  render() {
+    return <Container>LoadMore</Container>;
+  }
+}
+
+export default LoadMore;
---
+++ client/src/components/load-more/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 40px;
+  width: 100%;
+  background: pink;
+`;
