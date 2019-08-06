04dec897b4688285259461f3157b770408b3eb67
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-08-06 18:32:56 -0400

    notification list change

--- client/src/constants/notificationList.js
+++ client/src/constants/notificationList.js
@@ -1,40 +1,36 @@
-
-
-import {TRANSFER_SUCCEED} from "../actions/types";
-
+import { TRANSFER_SUCCEED } from "../actions/types";
 
 export const SUCCESS = "success";
 export const ERROR = "error";
 
 export const INCOMING_TRANSFER_MESSAGE = "incoming_transfer_message";
 
-export const notificationList =  [
+export const notificationList = [
+  {
+    key: TRANSFER_SUCCEED,
+    code: 0,
+    message: "Your transfer was successfully submitted...",
+    type: SUCCESS
+  },
+  {
+    key: INCOMING_TRANSFER_MESSAGE,
+    code: 0,
+    message: "Cha ching! You received a transfer...",
+    type: SUCCESS
+  },
 
-    {
-        "key": TRANSFER_SUCCEED,
-        "code": 0,
-        "message": "your transfer was submitted",
-        "type": SUCCESS
-    },
-    {
-        "key": INCOMING_TRANSFER_MESSAGE,
-        "code": 0,
-        "message": "a transfer to you is underway",
-        "type": SUCCESS
-    },
-
-    {
-        "key": "",
-        "code": -1,
-        "message": "wrong",
-        "type": ERROR
-    },
-    {
-        "key": "",
-        "code": -2,
-        "message": "you are doing it wrong",
-        "type": ERROR
-    }
-
-
+  {
+    key: "",
+    code: -1,
+    message: "Oops, that seed was incorrect",
+    type: ERROR
+    // Is this the vault login error?
+  },
+  {
+    key: "",
+    code: -2,
+    message: "you are doing it wrong",
+    type: ERROR
+    // What is this error for?
+  }
 ];
