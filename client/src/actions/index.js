2b864b6cef5dd0bf18c0df3f1bc67076089eab79
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-08-06 18:15:22 -0400

    cleaned code

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,89 +1,63 @@
-import {getAddressRPC} from "../rpc/rpc";
+import { getAddressRPC } from "../rpc/rpc";
 
 import {
-    GET_ADDRESS_FAILED,
-    GET_ADDRESS_FETCHING,
-    GET_ADDRESS_SUCCEED,
-
+  GET_ADDRESS_FAILED,
+  GET_ADDRESS_FETCHING,
+  GET_ADDRESS_SUCCEED
 } from "./types";
 
-export * from './prices';
-export * from './wallet';
-export * from './balance';
-export * from './transfer';
-export * from './key';
-export * from './theme';
-export * from './transferHistory';
-export * from './chain';
-
-
+export * from "./prices";
+export * from "./wallet";
+export * from "./balance";
+export * from "./transfer";
+export * from "./key";
+export * from "./theme";
+export * from "./transferHistory";
+export * from "./chain";
 
 export const getAddress = () => {
-
-    return(dispatch) => {
-
-        dispatch(getAddressFetching());
-        getAddressRPC()
-            .then(result => dispatch(getAddressSucceed(result)))
-            .catch(error => dispatch(getAddressFailed(error)));
-    }
+  return dispatch => {
+    dispatch(getAddressFetching());
+    getAddressRPC()
+      .then(result => dispatch(getAddressSucceed(result)))
+      .catch(error => dispatch(getAddressFailed(error)));
+  };
 };
 
-const getAddressFetching = () => ({type: GET_ADDRESS_FETCHING});
-const getAddressSucceed = (result) => ({type: GET_ADDRESS_SUCCEED, payload:result});
-const getAddressFailed = (error) => ({type: GET_ADDRESS_FAILED, payload: error});
-
+const getAddressFetching = () => ({ type: GET_ADDRESS_FETCHING });
+const getAddressSucceed = result => ({
+  type: GET_ADDRESS_SUCCEED,
+  payload: result
+});
+const getAddressFailed = error => ({
+  type: GET_ADDRESS_FAILED,
+  payload: error
+});
 
 export const getForex = () => {
+  // https://api.exchangeratesapi.io/latest?base=USD
+  // GET https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01 HTTP/1.1
 
+  //   https://api.exchangeratesapi.io/history?start_at=2019-07-23&end_at=2019-07-30&base=USD
 
-// https://api.exchangeratesapi.io/latest?base=USD
-// GET https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01 HTTP/1.1
+  const endDate = new Date();
+  const startDate = endDate - 14 * 3600 * 24;
+  const isoEndDate = endDate.toISOString().split("T")[0];
+  const isoStartDate = endDate.toISOString().split("T")[0];
 
- //   https://api.exchangeratesapi.io/history?start_at=2019-07-23&end_at=2019-07-30&base=USD
+  const forexUrl = `https://api.exchangeratesapi.io/history?start_at=${isoStartDate}&end_at=${isoEndDate}&base=USD`;
 
-    const endDate = new Date();
-    const startDate = endDate - 14 * 3600 * 24;
-    const isoEndDate = endDate.toISOString().split('T')[0];
-    const isoStartDate= endDate.toISOString().split('T')[0];
-
-    const forexUrl = `https://api.exchangeratesapi.io/history?start_at=${isoStartDate}&end_at=${isoEndDate}&base=USD`;
-
-
-    return(dispatch) => {
-        dispatch(getForexFetching());
-        fetch(forexUrl)
-            .then(res => res.json())
-            .then((res)=> dispatch(getForexSucceed(res)))
-            .catch(error => dispatch(getForexFailed(error)));
-    }
+  return dispatch => {
+    dispatch(getForexFetching());
+    fetch(forexUrl)
+      .then(res => res.json())
+      .then(res => dispatch(getForexSucceed(res)))
+      .catch(error => dispatch(getForexFailed(error)));
+  };
 };
 
+const getForexFetching = () => {};
 
-const getForexFetching = () => {
+const getForexSucceed = () => {};
 
-
-
-};
-
-
-const getForexSucceed = () => {
-
-
-
-
-};
-
-
-const getForexFailed = () => {
-
-
-
-};
-
-
-
-
-
-
-
+const getForexFailed = () => {};
