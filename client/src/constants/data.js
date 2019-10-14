e5dc9f968c965c009f182d5646ae3ac8090c0af0
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-10-14 17:12:35 -0500

    added the currencies

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -1,8 +1,67 @@
 module.exports = [
-
   {
     token: "United States Dollar",
     ticker: "xUSD",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "New Zealand Dollar",
+    ticker: "xNZD",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "Norwegian Krone",
+    ticker: "xNOK",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "Japanese Yen",
+    ticker: "xJPY",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "British Pound",
+    ticker: "xGBP",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "Euro",
+    ticker: "xEUR",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "Chinese Yuan",
+    ticker: "xCNY",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "Swiss Franc",
+    ticker: "xCHF",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "Canadian Dollar",
+    ticker: "xCAN",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "Bitcoin",
+    ticker: "xBTC",
+    price: "$1.00",
+    change: "~ 0.00"
+  },
+  {
+    token: "Australian Dollar",
+    ticker: "xAUD",
     price: "$1.00",
     change: "~ 0.00"
   }
