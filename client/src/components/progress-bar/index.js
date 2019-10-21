ffb15dee3ec546a6fdb4ce60e8eb4442da85a463
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-10-21 15:00:27 -0500

    refactored the percentage

--- client/src/components/progress-bar/index.js
+++ client/src/components/progress-bar/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Container, Progress } from "./styles";
 
-export const ProgressBar = ({ max, value }) => {
-  const amount = value / max;
-  const percentage = amount.toFixed(2);
-
+export const ProgressBar = ({ percentage }) => {
   return (
     <Container>
-      <Progress width={percentage * 100} />
+      <Progress width={percentage} />
     </Container>
   );
 };
