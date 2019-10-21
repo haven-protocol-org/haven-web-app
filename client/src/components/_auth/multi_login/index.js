8b1fb7460ee4bc5dc842853410edb6c9a292d9eb
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-10-21 15:24:34 -0500

    created multi-login component

---
+++ client/src/components/_auth/multi_login/index.js
@@ -0,0 +1,59 @@
+// Library Imports
+import React from "react";
+
+// Relative Imports
+import { Title, Description } from "../../../constants/type.js";
+import {
+  Container,
+  Main,
+  Header,
+  Buttons,
+  Submit,
+  Footer,
+  Cancel,
+  Body,
+  Route,
+  Label
+} from "./styles";
+import { Spinner } from "../../spinner";
+
+const MultiLogin = ({
+  title,
+  description,
+  children,
+  route,
+  onClick,
+  link,
+  label,
+  information,
+  submit,
+  cancel,
+  step,
+  width,
+  loading,
+  disable
+}) => {
+  return (
+    <Container>
+      <Header>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+      </Header>
+      <Main>
+        <Body>{children}</Body>
+        <Buttons>
+          <Cancel to="/">Cancel</Cancel>
+          <Submit disabled={disable} onClick={onClick}>
+            {loading ? <Spinner color={"white"} /> : "Login"}
+          </Submit>
+        </Buttons>
+      </Main>
+      <Footer>
+        <Label>{label}</Label>
+        <Route to={link}>{route}</Route>
+      </Footer>
+    </Container>
+  );
+};
+
+export default MultiLogin;
