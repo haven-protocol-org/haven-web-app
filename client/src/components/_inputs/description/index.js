6eed2a9b04616a3501d6829865db882b75941892
MartyHav <lala>
2019-10-25 17:56:31 +0200

    add iframe

--- client/src/components/_inputs/description/index.js
+++ client/src/components/_inputs/description/index.js
@@ -4,15 +4,19 @@ import React from "react";
 // Relative Imports
 import { Container, Field, Labels, Error } from "./styles";
 import { Label } from "../../../constants/type.js";
+import Auth from "../../../pages/_auth/login";
+
 
 const Description = ({ label, error, width, onClick, ref, type, ...rest }) => {
+
   return (
     <Container width={width}>
       <Labels>
         <Label>{label}</Label>
         <Error>{error}</Error>
       </Labels>
-      <Field ref={ref} {...rest} />
+        <iframe style={{border: 'none'}} src={"http://127.0.0.1:8080/field.html"}></iframe>
+        <Field ref={ref} {...rest} />
     </Container>
   );
 };
