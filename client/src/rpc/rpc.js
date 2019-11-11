0e9c96e1456d92dbfed9bceed716ef075a7c18dc
MartyHav <lala>
2019-11-11 08:59:49 +0100

    add rpc communication layer

---
+++ client/src/rpc/rpc.js
@@ -0,0 +1,90 @@
+const INIT_REQUEST = {
+  method: "POST",
+  mode: "cors",
+  cache: "no-cache",
+  credentials: "omit",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  redirect: "follow",
+  referrer: "no-referrer"
+};
+
+let sessionID = -1;
+
+let processingCalls = 0;
+
+function parseSessionID(result) {
+
+  sessionID = result.sessionid;
+  localStorage.setItem('sessionID', result.sessionid);
+
+  return result;
+}
+
+export function resetSessionId() {
+  localStorage.clear();
+  sessionID = -1;
+}
+
+export function restoreWalletRPC(params) {
+  return callRpc("restore_deterministic_wallet", params).then(parseSessionID);
+}
+
+export function getBalanceRPC(params) {
+  return callRpc("get_balance", params);
+}
+
+export function queryMnemonicKeyRPC() {
+    return callRpc("query_key", { key_type: "mnemonic" });
+}
+
+export function transferRPC(params) {
+  return callRpc("transfer", params);
+}
+
+export function getTransferRPC(params) {
+  return callRpc('get_transfers', params);
+}
+
+export function createWalletRPC(params) {
+  return callRpc("create_wallet", params).then(parseSessionID);
+}
+
+export function getHeightRPC() {
+  return callRpc('get_height');
+}
+
+export function refreshRPC(start_height = 0) {
+  return callRpc('refresh' ,{start_height});
+}
+
+function callRpc(method, params) {
+
+    const rpcUrl = process.env.REACT_APP_RPC_URL;
+    const objRequest = {
+        id: 0,
+        jsonrpc: "2.0",
+        method: method,
+        params: params
+    };
+
+    if (sessionID === -1) {
+        sessionID = localStorage.getItem('sessionID')? localStorage.getItem('sessionID') : -1;
+    }
+
+    if (sessionID !== -1) objRequest.sessionID = sessionID;
+
+    return fetch(rpcUrl, { ...INIT_REQUEST, body: JSON.stringify(objRequest) })
+        .then(response => response.json())
+        .then(function(response) {
+
+            processingCalls--;
+            const error = response.error || response.result.error;
+            if (error)
+                throw error;
+            return response.result;
+        });
+}
+
+
