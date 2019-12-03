9b72d0bf3971ff2737dd7004aee45c8bbd2585f0
Pierre Lafitte <pierrelafitte@protonmail.com>
2019-12-02 21:04:25 -0400

    minor change

--- client/src/platforms/desktop/pages/public/welcome/index.js
+++ client/src/platforms/desktop/pages/public/welcome/index.js
@@ -2,26 +2,20 @@
 import React, { Component } from "react";
 
 // Relative Imports
-import {
-  Container,
-  Microcopy,
-  Page
-} from "./styles";
-import {MultiLoginDesktop} from "../../_auth/multi_login";
-
+import { Container, Microcopy, Page } from "./styles";
+import { MultiLoginDesktop } from "../../../components/_auth/multi_login";
 
 export class WelcomeDesktop extends Component {
   componentDidMount() {
     window.scrollTo(0, 0);
   }
 
-
   render() {
     return (
       <Page>
         <Container>
           <Microcopy>
-        <MultiLoginDesktop/>
+            <MultiLoginDesktop />
           </Microcopy>
         </Container>
       </Page>
