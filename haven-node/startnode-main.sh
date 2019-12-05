979d2bf0822efd637e428828bbea4f2f287ae614
dev <juhu>
2019-12-05 09:07:26 +0100

    remove bins from develop branch

--- haven-node/startnode-main.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-trap 'kill %1;' SIGINT
-# ./mainnet/havend & sleep '5' &
-./mainnet/haven-wallet-rpc '--rpc-bind-port=12345' '--log-level=0' '--disable-rpc-login' '--wallet-dir=./' '--prompt-for-password'
