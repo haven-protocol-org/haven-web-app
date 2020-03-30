2374453687bbf4e84d66867e3deeec2762fcbcf1
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-03-29 21:10:58 -0400

    cleaning ipc

--- client/src/platforms/desktop/ipc/ipc-types.ts
+++ client/src/platforms/desktop/ipc/ipc-types.ts
@@ -1,30 +1,20 @@
-
-
 export enum CommunicationChannels {
-
-    RPC="rpc",
-    DAEMON="daemon",
-    WALLETS="wallets"
+  RPC = "rpc",
+  DAEMON = "daemon",
+  WALLETS = "wallets"
 }
 
-
-export interface DAEMON_STATUS  {
-
-    isRunning: boolean,
-    code?: number,
-    signal?: string,
-
+export interface DAEMON_STATUS {
+  isRunning: boolean;
+  code?: number;
+  signal?: string;
 }
-
 
 export interface DAEMON_STATUS_MESSAGE {
-    wallet: DAEMON_STATUS,
-    node:DAEMON_STATUS
+  wallet: DAEMON_STATUS;
+  node: DAEMON_STATUS;
 }
 
-
 export interface AVAILABLE_WALLETS {
-
-    wallets:{name:string,address: string}[];
-
-};
+  wallets: { name: string; address: string }[];
+}
