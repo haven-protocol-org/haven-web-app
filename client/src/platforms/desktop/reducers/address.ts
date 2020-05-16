28af0334104634b99af9326fe7053b09c4d4280a
MartyHav <mar.neuber@gmail.com>
2020-05-16 09:37:43 +0200

    convert into typescript

--- client/src/platforms/desktop/reducers/address.ts
+++ client/src/platforms/desktop/reducers/address.ts
@@ -1,8 +1,15 @@
 import { ADD_PUB_ADDRESS } from "../actions/types";
 
+type AdressEntry = {
+  label: string;
+  used: boolean;
+  address: string;
+  index: number;
+};
+
 const INITIAL_STATE = { main: null, subs: [] };
 
-export default function(state = INITIAL_STATE, action) {
+export default function (state = INITIAL_STATE, action: any) {
   switch (action.type) {
     case ADD_PUB_ADDRESS:
       return { ...state, main: action.payload };
