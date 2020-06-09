757051f3a0de0c3d8dc76d48853d9f2ddeef97c0
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-06-09 10:10:43 -0400

    Removed console warnings and dead code

--- client/src/shared/reducers/address.ts
+++ client/src/shared/reducers/address.ts
@@ -1,6 +1,4 @@
-import {GET_ADDRESS_SUCCEED} from "shared/actions/types";
-import {DesktopAppState} from "platforms/desktop/reducers";
-import {WebAppState} from "platforms/web/reducers";
+import { GET_ADDRESS_SUCCEED } from "shared/actions/types";
 
 export type AddressEntry = {
   label: string;
@@ -20,11 +18,10 @@ export default function (state = INITIAL_STATE, action: any): AddressEntry[] {
   }
 }
 
-
 export const selectPrimaryAddress = (adresses: AddressEntry[]): string => {
+  const primaryAddress = adresses.find(
+    (addressEntry) => addressEntry.address_index === 0
+  );
 
-    const primaryAddress = adresses.find((addressEntry) => addressEntry.address_index === 0);
-
-    return primaryAddress!.address
-
+  return primaryAddress!.address;
 };
