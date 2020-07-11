b7c6482412a991a05a47e241a86edfc23fa42fa3
MartyHav <mar.neuber@gmail.com>
2020-07-11 23:26:21 +0200

    brdige valid channels

--- haven-desktop-app/sites/preload/preload.js
+++ haven-desktop-app/sites/preload/preload.js
@@ -1,1 +1,19 @@
-window.ipcRenderer = require('electron').ipcRenderer;
+const {
+    contextBridge,
+    ipcRenderer
+} = require("electron");
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+    "ipcRenderer", {
+        invoke: (channel, data) => {
+            // whitelist channels
+            let validChannels = ["havend", "wallet-rpc", "rpc", "wallets"];
+            if (validChannels.includes(channel)) {
+                return ipcRenderer.invoke(channel, data);
+            }
+        },
+
+    }
+);
