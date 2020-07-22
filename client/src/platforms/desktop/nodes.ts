f2276f6ca5abeee13f16ea66d61f35e4963d22aa
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-07-22 08:26:05 -0400

    Changed the node object to allow for better ux

--- client/src/platforms/desktop/nodes.ts
+++ client/src/platforms/desktop/nodes.ts
@@ -1,18 +1,26 @@
 export const REMOTE_NODES = [
   {
     address: "http://remote.haven.miner.rocks",
+    name: "Haven Miner Rocks",
     port: "17750",
+    trusted: true,
   },
   {
-    address:'http://nodes.hashvault.pro',
+    address: "http://nodes.hashvault.pro",
+    name: "Hashvault",
     port: "17750",
+    trusted: true,
   },
   {
     address: "http://xhv-pst.minershive.ca",
+    name: "Miners Hive",
     port: "17750",
+    trusted: true,
   },
   {
-    address:'http://remote.eu.havenprotocol.org',
+    address: "http://remote.eu.havenprotocol.org",
+    name: "Haven European Node",
     port: "17750",
-  }
+    trusted: true,
+  },
 ];
