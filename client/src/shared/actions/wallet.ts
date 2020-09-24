21d154a76623c7ecf6e61db6f49e75e17cd8ab2e
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-09-24 09:58:31 -0400

    Cleaning up notification messages

--- client/src/shared/actions/wallet.ts
+++ client/src/shared/actions/wallet.ts
@@ -1,238 +1,259 @@
-import { IOpenWallet, IMonerRPCConnection, ICreateWallet } from "typings"
-import { getNetworkByName } from "constants/env"
-import { OPEN_WALLET_FETCHING, OPEN_WALLET_FAILED, OPEN_WALLET_SUCCEED, CREATE_WALLET_FAILED, CREATE_WALLET_SUCCEED, CREATE_WALLET_FETCHING, QUERY_MNEMONIC_FOR_WALLET_GENERATION_SUCCEED, VALIDATE_MNEMONIC_SUCCEED, VALIDATE_MNEMONIC_FAILED, RESTORE_WALLET_BY_SEED_FETCHING, RESTORE_WALLET_BY_SEED_SUCCEED, RESTORE_WALLET_BY_SEED_FAILED } from "platforms/desktop/actions/types"
-import { createWallet as createWalletCore, openWallet as openWalletCore, getMnemonic, addWalletListener, getOffshoreBalance, syncWallet, isWalletSynced, getNodeHeight, syncAtOnce, isWalletConnected, getWalletHeight, getChainHeight } from "../core/wallet"
-import { addErrorNotification, addNotificationByMessage } from "./notification"
-import { NotificationType } from "constants/notificationList"
-import { getXHVBalance, getXUSDBalance } from "./balance"
-import { getPrimaryAddress } from "./address"
-import { webWalletConnection } from "platforms/web/nodes"
-import { createDaemonConnection } from "./havend"
-import { updateHavenFeatures } from "./havenFeature"
-import { SET_WALLET_CONNECTION_STATE } from "./types"
-import { Chain } from "shared/reducers/chain"
-import { onWalletSyncUpdateSucceed } from "./chain"
-import { HavenAppState } from "platforms/desktop/reducers"
+import { IOpenWallet, IMonerRPCConnection, ICreateWallet } from "typings";
+import { getNetworkByName } from "constants/env";
+import {
+  OPEN_WALLET_FETCHING,
+  OPEN_WALLET_FAILED,
+  OPEN_WALLET_SUCCEED,
+  CREATE_WALLET_FAILED,
+  CREATE_WALLET_SUCCEED,
+  CREATE_WALLET_FETCHING,
+  QUERY_MNEMONIC_FOR_WALLET_GENERATION_SUCCEED,
+  VALIDATE_MNEMONIC_SUCCEED,
+  VALIDATE_MNEMONIC_FAILED,
+  RESTORE_WALLET_BY_SEED_FETCHING,
+  RESTORE_WALLET_BY_SEED_SUCCEED,
+  RESTORE_WALLET_BY_SEED_FAILED,
+} from "platforms/desktop/actions/types";
+import {
+  createWallet as createWalletCore,
+  openWallet as openWalletCore,
+  getMnemonic,
+  addWalletListener,
+  getOffshoreBalance,
+  syncWallet,
+  isWalletSynced,
+  getNodeHeight,
+  syncAtOnce,
+  isWalletConnected,
+  getWalletHeight,
+  getChainHeight,
+} from "../core/wallet";
+import { addErrorNotification, addNotificationByMessage } from "./notification";
+import { NotificationType } from "constants/notificationList";
+import { getXHVBalance, getXUSDBalance } from "./balance";
+import { getPrimaryAddress } from "./address";
+import { webWalletConnection } from "platforms/web/nodes";
+import { createDaemonConnection } from "./havend";
+import { updateHavenFeatures } from "./havenFeature";
+import { SET_WALLET_CONNECTION_STATE } from "./types";
+import { Chain } from "shared/reducers/chain";
+import { onWalletSyncUpdateSucceed } from "./chain";
+import { HavenAppState } from "platforms/desktop/reducers";
 
 /** collection of actions to open, create and store wallet */
 
-
-
-
 /** used by browser */
-export const openWalletByData = (keysData: Uint8Array, cacheData: Uint8Array, password: string, path: string) => {
+export const openWalletByData = (
+  keysData: Uint8Array,
+  cacheData: Uint8Array,
+  password: string,
+  path: string
+) => {
+  const walletData: IOpenWallet = {
+    keysData,
+    cacheData,
+    password,
+    networkType: getNetworkByName(),
+    server: webWalletConnection,
+  };
 
-    const walletData: IOpenWallet = {
-        keysData, cacheData, password, networkType:getNetworkByName(), server: webWalletConnection
-    }
-
-    return (dispatch: any) => {
-        dispatch(openWallet(walletData, path))
-    }
-    
-}
+  return (dispatch: any) => {
+    dispatch(openWallet(walletData, path));
+  };
+};
 
 /** used by nodejs env */
 export const openWalletByFile = (path: string, password: string) => {
+  const walletData: IOpenWallet = {
+    path,
+    password,
+    networkType: getNetworkByName(),
+    server: webWalletConnection,
+  };
 
-    const walletData: IOpenWallet = {
-        path, password, networkType:getNetworkByName(), server: webWalletConnection
-    }
-
-    return (dispatch: any) => {
-        dispatch(openWallet(walletData, path))
-    }
-}
-
+  return (dispatch: any) => {
+    dispatch(openWallet(walletData, path));
+  };
+};
 
 const openWallet = (walletData: IOpenWallet, path: string) => {
+  return async (dispatch: any) => {
+    dispatch(openWalletFetching);
 
+    const successOrError: boolean | object = await openWalletCore(walletData);
 
-    return async(dispatch: any) => {
-
-        dispatch(openWalletFetching);
-
-        const successOrError: boolean | object = await openWalletCore(walletData);
-
-        if (successOrError === true) {
-            dispatch(openWalletSucceed(path))
-            addNotificationByMessage(NotificationType.SUCCESS, 'wallet is open');
-            dispatch(startWalletSession());
-        }
-        else {
-            addNotificationByMessage(NotificationType.ERROR, 'open wallet is not working, pls try again tomorrow');
-            dispatch(openWalletFailed(successOrError as object))
-         }
+    if (successOrError === true) {
+      dispatch(openWalletSucceed(path));
+      addNotificationByMessage(NotificationType.SUCCESS, "Vault is open");
+      dispatch(startWalletSession());
+    } else {
+      addNotificationByMessage(
+        NotificationType.ERROR,
+        "Open vault is not working, please try again soon..."
+      );
+      dispatch(openWalletFailed(successOrError as object));
     }
-}
+  };
+};
 
 /** store wallet data as file */
-export const storeWalletData = () => {
-
-
-
-}
-
-
+export const storeWalletData = () => {};
 
 export const createNewWallet = (path: string, password: string) => {
+  const walletData: ICreateWallet = {
+    path,
+    password,
+    server: webWalletConnection,
+    networkType: getNetworkByName(),
+  };
 
-    const walletData: ICreateWallet = {
-        path, password, server: webWalletConnection, networkType: getNetworkByName()
+  return async (dispatch: any) => {
+    dispatch(createWalletFetch());
+    const successOrError: boolean | object = await createWalletCore(walletData);
+
+    if (successOrError === true) {
+      const mnemomic = getMnemonic();
+      dispatch(queryMnemonicForWalletGenerationSucceed(mnemomic));
+      dispatch(createWalletSucceed());
+      addNotificationByMessage(NotificationType.SUCCESS, "Vault is open");
+    } else {
+      addNotificationByMessage(
+        NotificationType.ERROR,
+        "Open vault is not working, please try again soon..."
+      );
+      dispatch(createWalletFailed(successOrError as object));
     }
-
-    return async(dispatch: any) => {
-
-
-        dispatch(createWalletFetch())
-        const successOrError: boolean | object = await createWalletCore(walletData);
-
-
-        if (successOrError === true) {
-            const mnemomic = getMnemonic();
-            dispatch(queryMnemonicForWalletGenerationSucceed(mnemomic));
-            dispatch(createWalletSucceed())
-            addNotificationByMessage(NotificationType.SUCCESS, 'wallet is open');
-        }
-        else {
-            addNotificationByMessage(NotificationType.ERROR, 'open wallet is not working, pls try again tomorrow');
-            dispatch(createWalletFailed(successOrError as object))
-         }
-    }
-}
+  };
+};
 
 export const restoreWalletByMnemomic = (mnemonic: string, password: string) => {
+  const walletData: ICreateWallet = {
+    mnemonic,
+    password,
+    networkType: getNetworkByName(),
+    server: webWalletConnection,
+  };
 
-    const walletData:ICreateWallet = {
-        mnemonic, password, networkType: getNetworkByName(), server: webWalletConnection
+  return async (dispatch: any) => {
+    dispatch(restoreWalletFetching());
+    const successOrError: boolean | object = await createWalletCore(walletData);
+
+    if (successOrError === true) {
+      dispatch(restoreWalletSucceed());
+      addNotificationByMessage(
+        NotificationType.SUCCESS,
+        "Restored vault with Mnemomic"
+      );
+      dispatch(startWalletSession());
+    } else {
+      addNotificationByMessage(
+        NotificationType.ERROR,
+        "Restore vault is not working, please try again soon..."
+      );
+      dispatch(restoreWalletFailed(successOrError));
     }
+  };
+};
 
-    return async(dispatch: any) => {
-        dispatch(restoreWalletFetching())
-        const successOrError: boolean | object = await createWalletCore(walletData);
+export const restoreWalletByKeys = (
+  primaryAddress: string,
+  privateSpendKey: string,
+  privateVieKey: string,
+  password: string
+) => {
+  const walletData: ICreateWallet = {
+    networkType: getNetworkByName(),
+    server: webWalletConnection,
+    password,
+  };
 
-        if (successOrError === true) {
-            dispatch(restoreWalletSucceed());
-            addNotificationByMessage(NotificationType.SUCCESS, 'restored wallet by Mnemomic');
-            dispatch(startWalletSession());
-            
-        }
-        else {
-            addNotificationByMessage(NotificationType.ERROR, 'restore wallet is not working, pls try again tomorrow');
-            dispatch(restoreWalletFailed(successOrError));
-         }
+  return async (dispatch: any) => {
+    dispatch(createWalletFetch());
+    const successOrError: boolean | object = await createWalletCore(walletData);
 
+    if (successOrError === true) {
+      dispatch(restoreWalletSucceed());
+      addNotificationByMessage(
+        NotificationType.SUCCESS,
+        "Restored vault with Keystore"
+      );
+      dispatch(startWalletSession());
+    } else {
+      addNotificationByMessage(
+        NotificationType.ERROR,
+        "Open vault is not working, please try again soon..."
+      );
+      dispatch(restoreWalletFailed(successOrError));
     }
-
-
-}
-
-export const restoreWalletByKeys = (primaryAddress: string, privateSpendKey: string, privateVieKey: string, password: string) => {
-
-    const walletData:ICreateWallet = {
-    networkType: getNetworkByName(), server: webWalletConnection, password
-    }
-
-    return async(dispatch: any) => {
-
-        dispatch(createWalletFetch())
-        const successOrError: boolean | object = await createWalletCore(walletData);
-
-
-        if (successOrError === true) {
-            dispatch(restoreWalletSucceed());
-            addNotificationByMessage(NotificationType.SUCCESS, 'restored wallet by keys');
-            dispatch(startWalletSession());
-            
-        }
-        else {
-            addNotificationByMessage(NotificationType.ERROR, 'open wallet is not working, pls try again tomorrow');
-            dispatch(restoreWalletFailed(successOrError));
-         }
-    }
-}
+  };
+};
 
 export const mnenomicVerificationSucceed = (fileName: string) => {
-    
-    
-    return (dispatch: any) => {
-     dispatch({
-            type: VALIDATE_MNEMONIC_SUCCEED,
-            payload: fileName,
-          })
+  return (dispatch: any) => {
+    dispatch({
+      type: VALIDATE_MNEMONIC_SUCCEED,
+      payload: fileName,
+    });
 
-          dispatch(startWalletSession());
-        
-    }
-    
+    dispatch(startWalletSession());
+  };
 };
-  export const mneomicVerifcationFailed = () => ({
-    type: VALIDATE_MNEMONIC_FAILED,
-  });
-
+export const mneomicVerifcationFailed = () => ({
+  type: VALIDATE_MNEMONIC_FAILED,
+});
 
 const startWalletSession = () => {
+  return async (dispatch: any, getStore: () => HavenAppState) => {
+    // initialize own connection to daemon ( needed for fetching block headers )
+    dispatch(createDaemonConnection());
 
-    return async (dispatch: any, getStore:() => HavenAppState) => {
+    await dispatch(initReduxWallet());
 
-        // initialize own connection to daemon ( needed for fetching block headers ) 
-        dispatch(createDaemonConnection());
+    // start wallet listeners
+    addWalletListener(dispatch, getStore);
+    syncWallet();
+  };
+};
 
-        await dispatch(initReduxWallet());
-    
-        // start wallet listeners
-        addWalletListener(dispatch, getStore);
-        syncWallet();
-
-    }
-}
-
-
-// init some basic data before wallet listener 
+// init some basic data before wallet listener
 // will be responsible for data updates
 const initReduxWallet = () => {
+  return async (dispatch: any) => {
+    dispatch(getXHVBalance());
+    dispatch(getXUSDBalance());
 
+    dispatch(getPrimaryAddress());
+    const isConnected = await isWalletConnected();
+    const isSynced = await isWalletSynced();
+    dispatch(setWalletConnectionState(isConnected));
+    const chainHeight = await getChainHeight();
+    const nodeHeight = await getNodeHeight();
+    const walletHeight = await getWalletHeight();
 
-    return async(dispatch: any) => {
+    const chainHeights: Partial<Chain> = {
+      walletHeight,
+      nodeHeight,
+      chainHeight,
+    } as Partial<Chain>;
 
-        dispatch(getXHVBalance());
-        dispatch(getXUSDBalance());
+    dispatch(onWalletSyncUpdateSucceed(chainHeights));
+    dispatch(updateHavenFeatures(nodeHeight));
 
-        dispatch(getPrimaryAddress());
-        const isConnected = await isWalletConnected();
-        const isSynced = await isWalletSynced();
-        dispatch(setWalletConnectionState(isConnected));
-        const chainHeight = await getChainHeight();
-        const nodeHeight = await getNodeHeight();
-        const walletHeight = await getWalletHeight();
-
-        const chainHeights: Partial<Chain> = {
-            walletHeight, nodeHeight, chainHeight
-        } as Partial<Chain>
-        
-        dispatch(onWalletSyncUpdateSucceed(chainHeights))
-        dispatch(updateHavenFeatures(nodeHeight));
-
-        return;
-
-
-
-    }
-        
-
-}
+    return;
+  };
+};
 
 const openWalletFetching = () => {
-    return { type: OPEN_WALLET_FETCHING };
+  return { type: OPEN_WALLET_FETCHING };
 };
 
 const openWalletSucceed = (fileName: string) => {
-    return { type: OPEN_WALLET_SUCCEED, payload: fileName };
+  return { type: OPEN_WALLET_SUCCEED, payload: fileName };
 };
-  
+
 const openWalletFailed = (error: object) => {
-    return { type: OPEN_WALLET_FAILED, payload: error };
-}
+  return { type: OPEN_WALLET_FAILED, payload: error };
+};
 
 const createWalletFetch = () => ({ type: CREATE_WALLET_FETCHING });
 
@@ -240,23 +261,20 @@ const createWalletSucceed = () => ({
   type: CREATE_WALLET_SUCCEED,
 });
 const createWalletFailed = (error: object) => ({
-  type: CREATE_WALLET_FAILED, payload: error
+  type: CREATE_WALLET_FAILED,
+  payload: error,
 });
 
 const queryMnemonicForWalletGenerationSucceed = (key: string) => ({
-    type: QUERY_MNEMONIC_FOR_WALLET_GENERATION_SUCCEED,
-    payload: key,
-  });
-  
+  type: QUERY_MNEMONIC_FOR_WALLET_GENERATION_SUCCEED,
+  payload: key,
+});
 
-  const setWalletConnectionState = (isConnected: boolean) => {
+const setWalletConnectionState = (isConnected: boolean) => {
+  return { type: SET_WALLET_CONNECTION_STATE, payload: isConnected };
+};
 
-    return {type: SET_WALLET_CONNECTION_STATE, payload: isConnected};
-
-  }
-
-
-  const restoreWalletFetching = () => ({ type: RESTORE_WALLET_BY_SEED_FETCHING });
+const restoreWalletFetching = () => ({ type: RESTORE_WALLET_BY_SEED_FETCHING });
 const restoreWalletSucceed = () => ({
   type: RESTORE_WALLET_BY_SEED_SUCCEED,
 });
--- client/src/shared/actions/walletListener.ts
+++ client/src/shared/actions/walletListener.ts
@@ -4,131 +4,130 @@ import BigInteger from "haven-wallet-core/src/main/js/common/biginteger";
 import { Balance } from "shared/reducers/xBalance";
 import { getBalancesSucceed } from "./balance";
 import { Chain, selectSyncState } from "shared/reducers/chain";
-import {  onWalletSyncUpdateSucceed } from "shared/actions/chain";
+import { onWalletSyncUpdateSucceed } from "shared/actions/chain";
 import { getLastBlockHeader } from "./blockHeaderExchangeRate";
 import { updateHavenFeatures } from "./havenFeature";
 import { getAllTransfers } from "./transferHistory";
 import { HavenAppState } from "platforms/desktop/reducers";
 
+export class HavenWalletListener extends MoneroWalletListener {
+  // we keep a dispatch and getStore instance in the walletlistener
+  // - not sure if its a good praxis
 
+  private getStore: () => HavenAppState;
+  private dispatch: any;
+  constructor(dispatch: any, getStore: () => HavenAppState) {
+    super();
+    this.dispatch = dispatch;
+    this.getStore = getStore;
+  }
 
-export class HavenWalletListener extends MoneroWalletListener  {
-
-    // we keep a dispatch and getStore instance in the walletlistener 
-    // - not sure if its a good praxis
-
-    private getStore:() => HavenAppState
-    private dispatch: any;
-    constructor(dispatch: any, getStore:() => HavenAppState) {
-        super();
-        this.dispatch = dispatch;
-        this.getStore = getStore;
+  /**
+   * Invoked as the wallet is synchronized.
+   *
+   * @param {number} height - height of the synced block
+   * @param {number} startHeight - starting height of the sync request
+   * @param {number} endHeight - ending height of the sync request
+   * @param {number} percentDone - sync progress as a percentage
+   * @param {string} message - human-readable description of the current progress
+   */
+  onSyncProgress(
+    height: number,
+    startHeight: number,
+    endHeight: number,
+    percentDone: number,
+    message: string
+  ): void {
+    if (
+      height <= startHeight + 2 ||
+      (height + 1000 < endHeight && height % 100 === 0) ||
+      (height + 30 < endHeight && height % 10 === 0) ||
+      height + 30 >= endHeight
+    ) {
+      const chain: Partial<Chain> = {
+        walletHeight: height,
+        // chainHeight:endHeight
+      };
+      this.dispatch(onWalletSyncUpdateSucceed(chain));
     }
+  }
+  /**
+   * Invoked when a new block is added to the chain.
+   *
+   * @param {int} height - the height of the block added to the chain
+   */
+  onNewBlock(height: any): void {
+    const store: HavenAppState = this.getStore();
+    const syncState = selectSyncState(store);
 
+    if (!syncState.isSyncing) {
+      this.dispatch(getLastBlockHeader());
+      this.dispatch(updateHavenFeatures(height));
 
-    /**
-     * Invoked as the wallet is synchronized.
-     *
-     * @param {number} height - height of the synced block
-     * @param {number} startHeight - starting height of the sync request
-     * @param {number} endHeight - ending height of the sync request
-     * @param {number} percentDone - sync progress as a percentage
-     * @param {string} message - human-readable description of the current progress
-     */
-    onSyncProgress(height: number, startHeight: number, endHeight: number, percentDone: number, message: string): void {
-
-       if ( height <= startHeight + 2
-        || height + 1000 < endHeight && height%100 === 0 
-        || height + 30 < endHeight  && height%10 === 0 
-        || height + 30 >= endHeight ) {
-
-            const chain: Partial<Chain> = {
-           
-                walletHeight:height,
-               // chainHeight:endHeight
-               }
-               this.dispatch(onWalletSyncUpdateSucceed(chain));
-        }
+      if (store.chain.chainHeight < height) {
+        this.dispatch(
+          onWalletSyncUpdateSucceed({ nodeHeight: height, chainHeight: height })
+        );
+      } else {
+        this.dispatch(onWalletSyncUpdateSucceed({ nodeHeight: height }));
+      }
     }
-    /**
-     * Invoked when a new block is added to the chain.
-     *
-     * @param {int} height - the height of the block added to the chain
-     */
-    onNewBlock(height: any): void {
-
-       const store: HavenAppState = this.getStore();
-       const syncState = selectSyncState(store);
-
-       if (!syncState.isSyncing) {
-
-        this.dispatch(getLastBlockHeader());
-        this.dispatch(updateHavenFeatures(height));
-
-        if (store.chain.chainHeight < height) {
-            this.dispatch(onWalletSyncUpdateSucceed({nodeHeight: height, chainHeight:height}));
-        } else {
-            this.dispatch(onWalletSyncUpdateSucceed({nodeHeight: height}));
-        }
-
-       
-
-       }
-    }
-    /**
-     * Invoked when the wallet's balances change.
-     *
-     * @param {BigInteger} newBalance - new wallet balance
-     * @param {BigInteger} newUnlockedBalance - new unlocked wallet balance
-     */
-    // @ts-ignore
-    onBalancesChanged(newBalance: BigInteger, newUnlockedBalance: BigInteger): void {
-
-        const balance = bigIntegerToBigInt(newBalance);
-        const unlockedBalance = bigIntegerToBigInt(newUnlockedBalance);
-        const xhvBalance: Balance = {
-            unlockedBalance,balance,lockedBalance:balance.subtract(unlockedBalance)
-        }
-        this.dispatch(getBalancesSucceed({XHV:xhvBalance}))
-       // this.dispatch(getAllTransfers());
-
-    }
-       /**
-     * Invoked when the wallet's offshore balances change.
-     *
-     * @param {BigInteger} newBalance - new wallet balance
-     * @param {BigInteger} newUnlockedBalance - new unlocked wallet balance
-     */
-    onOffshoreBalancesChanged(newBalance: BigInteger, newUnlockedBalance: BigInteger): void {
-
-        const balance = bigIntegerToBigInt(newBalance);
-        const unlockedBalance = bigIntegerToBigInt(newUnlockedBalance);
-        const xUSDBalance: Balance = {
-            unlockedBalance,balance,lockedBalance:balance.subtract(unlockedBalance)
-        }
-        this.dispatch(getBalancesSucceed({xUSD:xUSDBalance}))
-      //  this.dispatch(getAllTransfers());
-
-
-    }
-    /**
-     * Invoked when the wallet receives an output.
-     *
-     * @param {MoneroOutputWallet} output - the received output
-     */
-    onOutputReceived(output: any): void {
-
-        console.log('hey you received some money');
-
-    }
-    /**
-     * Invoked when the wallet spends an output.
-     *
-     * @param {MoneroOutputWallet} output - the spent output
-     */
-    onOutputSpent(output: any): void {
-
-        console.log('hey you sent some money');
-
-    }
+  }
+  /**
+   * Invoked when the wallet's balances change.
+   *
+   * @param {BigInteger} newBalance - new wallet balance
+   * @param {BigInteger} newUnlockedBalance - new unlocked wallet balance
+   */
+  // @ts-ignore
+  onBalancesChanged(
+    newBalance: BigInteger,
+    newUnlockedBalance: BigInteger
+  ): void {
+    const balance = bigIntegerToBigInt(newBalance);
+    const unlockedBalance = bigIntegerToBigInt(newUnlockedBalance);
+    const xhvBalance: Balance = {
+      unlockedBalance,
+      balance,
+      lockedBalance: balance.subtract(unlockedBalance),
+    };
+    this.dispatch(getBalancesSucceed({ XHV: xhvBalance }));
+    // this.dispatch(getAllTransfers());
+  }
+  /**
+   * Invoked when the wallet's offshore balances change.
+   *
+   * @param {BigInteger} newBalance - new wallet balance
+   * @param {BigInteger} newUnlockedBalance - new unlocked wallet balance
+   */
+  onOffshoreBalancesChanged(
+    newBalance: BigInteger,
+    newUnlockedBalance: BigInteger
+  ): void {
+    const balance = bigIntegerToBigInt(newBalance);
+    const unlockedBalance = bigIntegerToBigInt(newUnlockedBalance);
+    const xUSDBalance: Balance = {
+      unlockedBalance,
+      balance,
+      lockedBalance: balance.subtract(unlockedBalance),
+    };
+    this.dispatch(getBalancesSucceed({ xUSD: xUSDBalance }));
+    //  this.dispatch(getAllTransfers());
+  }
+  /**
+   * Invoked when the wallet receives an output.
+   *
+   * @param {MoneroOutputWallet} output - the received output
+   */
+  onOutputReceived(output: any): void {
+    console.log("Hey you received some money");
+  }
+  /**
+   * Invoked when the wallet spends an output.
+   *
+   * @param {MoneroOutputWallet} output - the spent output
+   */
+  onOutputSpent(output: any): void {
+    console.log("Hey you sent some money");
+  }
 }
