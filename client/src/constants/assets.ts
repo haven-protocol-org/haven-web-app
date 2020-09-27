b40ed9f05bd0d072e59266835766c9ee8e43d996
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-09-27 09:59:22 -0400

    Removing old xAssets

--- client/src/constants/assets.ts
+++ client/src/constants/assets.ts
@@ -6,41 +6,7 @@ export const AssetList = [
     change: "~ 0.00",
     symbol: "$",
   },
-  {
-    token: "Bitcoin",
-    ticker: "BTC",
-    price: "$1.00",
-    change: "~ 0.00",
-    symbol: "$",
-  },
-  {
-    token: "New Zealand Dollar",
-    ticker: "NZD",
-    price: "$1.00",
-    change: "~ 0.00",
-    symbol: "$",
-  },
-  {
-    token: "Norwegian Krone",
-    ticker: "NOK",
-    price: "$1.00",
-    change: "~ 0.00",
-    symbol: "kr",
-  },
-  {
-    token: "Japanese Yen",
-    ticker: "JPY",
-    price: "$1.00",
-    change: "~ 0.00",
-    symbol: "¥",
-  },
-  {
-    token: "British Pound",
-    ticker: "GBP",
-    price: "$1.00",
-    change: "~ 0.00",
-    symbol: "£",
-  },
+
   {
     token: "Euro",
     ticker: "EUR",
@@ -55,28 +21,7 @@ export const AssetList = [
     change: "~ 0.00",
     symbol: "¥",
   },
-  {
-    token: "Swiss Franc",
-    ticker: "CHF",
-    price: "$1.00",
-    change: "~ 0.00",
-    symbol: "₣",
-  },
-  {
-    token: "Canadian Dollar",
-    ticker: "CAD",
-    price: "$1.00",
-    change: "~ 0.00",
-    symbol: "$",
-  },
 
-  {
-    token: "Australian Dollar",
-    ticker: "AUD",
-    price: "$1.00",
-    change: "~ 0.00",
-    symbol: "$",
-  },
   {
     token: "Gold",
     ticker: "GOLD",
@@ -92,3 +37,61 @@ export const AssetList = [
     symbol: "$",
   },
 ];
+
+// {
+//   token: "Bitcoin",
+//   ticker: "BTC",
+//   price: "$1.00",
+//   change: "~ 0.00",
+//   symbol: "$",
+// },
+// {
+//   token: "Swiss Franc",
+//   ticker: "CHF",
+//   price: "$1.00",
+//   change: "~ 0.00",
+//   symbol: "₣",
+// },
+// {
+//   token: "Canadian Dollar",
+//   ticker: "CAD",
+//   price: "$1.00",
+//   change: "~ 0.00",
+//   symbol: "$",
+// },
+//
+// {
+//   token: "Australian Dollar",
+//   ticker: "AUD",
+//   price: "$1.00",
+//   change: "~ 0.00",
+//   symbol: "$",
+// },
+// {
+//   token: "New Zealand Dollar",
+//   ticker: "NZD",
+//   price: "$1.00",
+//   change: "~ 0.00",
+//   symbol: "$",
+// },
+// {
+//   token: "Norwegian Krone",
+//   ticker: "NOK",
+//   price: "$1.00",
+//   change: "~ 0.00",
+//   symbol: "kr",
+// },
+// {
+//   token: "Japanese Yen",
+//   ticker: "JPY",
+//   price: "$1.00",
+//   change: "~ 0.00",
+//   symbol: "¥",
+// },
+// {
+//   token: "British Pound",
+//   ticker: "GBP",
+//   price: "$1.00",
+//   change: "~ 0.00",
+//   symbol: "£",
+// },
