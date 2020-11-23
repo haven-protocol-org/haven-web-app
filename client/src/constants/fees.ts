4db732d96eb05858263cf02c1a70bde51a510874
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-11-23 11:42:13 -0400

    Fix fee time display copy and add a global fee array for implementation

---
+++ client/src/constants/fees.ts
@@ -0,0 +1,6 @@
+export const ExchangeFees = [
+  { name: "Default", ticker: "Unlocks ~7d", percent: "0.2%", prio: 0 },
+  { name: "Low", ticker: "Unlocks ~48h", percent: "5%", prio: 1 },
+  { name: "Medium", ticker: "Unlocks ~24hr", percent: "10%", prio: 2 },
+  { name: "High", ticker: "Unlocks ~6hr", percent: "20%", prio: 3 },
+];
--- client/src/shared/pages/_wallet/exchange/index.tsx
+++ client/src/shared/pages/_wallet/exchange/index.tsx
@@ -84,9 +84,9 @@ const assetOptions: AssetOption[] = [
 
 const exchangePrioOptions: ExchangePrioOption[] = [
   { name: "Default", ticker: "Unlocks ~7d", percent: "0.2%", prio: 0 },
-  { name: "Low", ticker: "Unlocks ~48d", percent: "5%", prio: 1 },
-  { name: "Medium", ticker: "Unlocks ~24hr", percent: "10%", prio: 2 },
-  { name: "High", ticker: "Unlocks ~6hr", percent: "20%", prio: 3 },
+  { name: "Low", ticker: "Unlocks ~48h", percent: "5%", prio: 1 },
+  { name: "Medium", ticker: "Unlocks ~24h", percent: "10%", prio: 2 },
+  { name: "High", ticker: "Unlocks ~6h", percent: "20%", prio: 3 },
 ];
 
 const INITIAL_STATE: ExchangeState = {
