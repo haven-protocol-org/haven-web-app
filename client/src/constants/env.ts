31dc4b605b0869f80c8fc7f5b81226e8aa9232a5
MartyHav <mar.neuber@gmail.com>
2020-11-26 18:50:37 +0100

    cleanup/enhance start scripts

--- client/src/constants/env.ts
+++ client/src/constants/env.ts
@@ -2,7 +2,7 @@ import { NetworkType } from "../typings";
 
 export const NET_TYPE_ID = parseInt(process.env.REACT_APP_NET_TYPE_ID!);
 export const APP_VERSION = process.env.REACT_APP_VERSION;
-export const NET_TYPE_NAME = process.env.REACT_APP_NET_TYPE_NAME;
+export const NET_TYPE_NAME = ["Mainnet", "Testnet", "Stagenet"][NET_TYPE_ID];
 export const MODE = process.env.NODE_ENV;
 
 export const DEV_MODE = "development";
