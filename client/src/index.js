f39e956a328e77a7f2c07c96118a020a5fde8222
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-12-07 11:51:20 -0400

    save and format. nothign to see here.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import { isDesktop, isDevMode } from "./constants/env";
 
 if (isDesktop()) {
-  import("./platforms/desktop").then(desktop => {
+  import("./platforms/desktop").then((desktop) => {
     isDevMode()
       ? desktop.startDesktopAppInDevMode()
       : desktop.startDesktopApp();
   });
 } else {
-  import("./platforms/web").then(web => {
+  import("./platforms/web").then((web) => {
     isDevMode() ? web.startWebAppInDevMode() : web.startWebApp();
   });
 }
