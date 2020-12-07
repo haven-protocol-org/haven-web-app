e8c41f2726f98ed2398f074c0606b6e6a670982f
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-12-07 13:12:44 -0400

    Add a login onboarding modal for users

---
+++ client/src/shared/components/tutorial/login/index.js
@@ -0,0 +1,38 @@
+// Library Imports
+import React from "react";
+
+// Relative Imports
+import { Container, List, Item } from "./styles";
+
+const LoginTutorial = () => {
+  return (
+    <Container>
+      <>
+        <List>First Option</List>
+        <Item>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </Item>
+      </>
+      <>
+        <List>Second Option</List>
+        <Item>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </Item>
+      </>
+      <>
+        <List>Third Option</List>
+        <Item>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation
+        </Item>
+      </>
+    </Container>
+  );
+};
+
+export default LoginTutorial;
