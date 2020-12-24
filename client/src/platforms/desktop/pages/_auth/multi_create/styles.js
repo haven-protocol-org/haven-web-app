030f8f098731e97367fa70cdb0620eab28b79b49
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-12-23 22:25:48 -0400

    Fixed the auth screen themes.

--- client/src/platforms/desktop/pages/_auth/multi_create/styles.js
+++ client/src/platforms/desktop/pages/_auth/multi_create/styles.js
@@ -7,34 +7,29 @@ export const Container = styled.div`
   max-width: 624px;
   flex-direction: column;
   margin: 20px;
-  border: 1px solid #3a4048;
+  border: 1px solid ${(props) => props.theme.body.border};
   border-radius: 4px;
   box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.05);
 `;
 
 export const Wrapper = styled.div`
   height: 100%;
-  width: auto;
-  background: green;
 `;
 
 export const Main = styled.div`
-  background: #36393f;
+  background: ${(props) => props.theme.body.background};
   min-height: 350px;
-  height: auto;
   padding: 10px;
 `;
 
 export const Body = styled.div`
   min-height: 266px;
-  width: auto;
   margin: 10px;
 `;
 
 export const Tabs = styled.div`
   height: 40px;
   width: 100%;
-  background: #36393f;
   display: flex;
 `;
 
@@ -45,33 +40,32 @@ export const Tab = styled.div`
   align-items: center;
   justify-content: center;
   font-size: 13px;
-  color:  ${(props) => (props.active ? "#fff" : "#8a8d90")};
-  background: ${(props) => (props.active ? "#36393f" : "#313339")};
-
-
+  color:  ${(props) =>
+    props.active ? props.theme.type.primary : props.theme.type.secondary};
+  background: ${(props) =>
+    props.active ? props.theme.body.background : props.theme.body.foreground};
   border-top: none;
   transition: 500ms;
 
   &:hover {
   cursor: pointer;
   transition: 500ms;
-  color: white;
+  color: ${(props) => props.theme.type.primary};
   }
 
 `;
 
 export const Header = styled.div`
-  height: auto;
-  width: auto;
   padding: 20px;
-  background: #36393f;
-  border-bottom: 1px solid #3a4048;
+  border-radius: 3px 3px 0px 0px;
+  background: ${(props) => props.theme.body.background};
+  border-bottom: 1px solid ${(props) => props.theme.body.border};
 `;
 
 export const Title = styled.div`
   font-family: Inter-SemiBold;
   font-size: 20px;
-  color: #fff;
+  color: ${(props) => props.theme.type.primary};
   letter-spacing: 0;
   line-height: 30px;
 `;
@@ -79,16 +73,8 @@ export const Title = styled.div`
 export const Description = styled.div`
   font-family: Inter-Regular;
   font-size: 14px;
-  color: #8a8d90;
+  color: ${(props) => props.theme.type.secondary};
   line-height: 24px;
-`;
-
-export const Progress = styled.div`
-  display: grid;
-  background: #2b2e34;
-  height: 4px;
-  width: 100%;
-  margin-bottom: 16px;
 `;
 
 export const Step = styled.div`
@@ -101,107 +87,22 @@ export const Step = styled.div`
   border-radius: 4px;
 `;
 
-export const Buttons = styled.div`
-  height: auto;
-  margin: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-export const Submit = styled.button`
-  background: #7289da;
-  border-radius: 4px;
-  font-family: Inter-Regular;
-  font-size: 14px;
-  color: #ffffff;
-  text-align: center;
-  line-height: 24px;
-  width: 106px;
-  height: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: 500ms;
-  border: none;
-  outline: none;
-
-  &:hover {
-    cursor: pointer;
-    transition: 500ms;
-    background: #5b6eae;
-  }
-
-  &:disabled {
-    color: rgba(255, 255, 255, 0.5);
-    cursor: not-allowed;
-  }
-`;
-
-export const Back = styled.div`
-  background: #36393f;
-  border-radius: 4px;
-  font-family: Inter-Regular;
-  font-size: 14px;
-  color: #ffffff;
-  text-align: center;
-  line-height: 24px;
-  width: 106px;
-  height: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid #3a4048;
-  text-decoration: none;
-  transition: 500ms;
-
-  &:hover {
-    cursor: pointer;
-    background: #313339;
-    transition: 500ms;
-  }
-`;
-
-export const Cancel = styled(Link)`
-  background: #36393f;
-  border-radius: 4px;
-  font-family: Inter-Regular;
-  font-size: 14px;
-  color: #ffffff;
-  text-align: center;
-  line-height: 24px;
-  width: 106px;
-  height: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid #3a4048;
-  text-decoration: none;
-  transition: 500ms;
-
-  &:hover {
-    cursor: pointer;
-    background: #313339;
-    transition: 500ms;
-  }
-`;
-
 export const Footer = styled.div`
   height: 60px;
-  border-top: 1px solid #3a4048;
+  border-top: 1px solid ${(props) => props.theme.body.border};
   text-align: center;
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
-  background: #36393f;
+  background: ${(props) => props.theme.body.background};
   border-radius: 0px 0px 4px 4px;
 `;
 
 export const Route = styled(Link)`
   font-family: Inter-SemiBold;
   font-size: 14px;
-  color: #fff;
+  color: ${(props) => props.theme.type.primary};
   text-decoration: none;
   margin-left: 8px;
 `;
@@ -209,6 +110,6 @@ export const Route = styled(Link)`
 export const Label = styled.div`
   font-family: Inter-Regular;
   font-size: 14px;
-  color: #8a8d90;
+  color: ${(props) => props.theme.type.secondary};
   text-decoration: none;
 `;
