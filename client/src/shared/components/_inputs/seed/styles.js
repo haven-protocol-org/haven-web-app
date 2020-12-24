cccbb8403cef4f16e2078492271bbaa424dda8ca
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-12-24 11:16:26 -0400

    remove static styles and nuke dead code

--- client/src/shared/components/_inputs/seed/styles.js
+++ client/src/shared/components/_inputs/seed/styles.js
@@ -1,43 +1,13 @@
 import styled from "styled-components";
-import media from "../../../../assets/styles/media.js";
 
 export const Error = styled.div`
   font-family: Inter-Regular;
   font-size: 14px;
-  color: #f04747;
+  color: ${(props) => props.theme.input.input_error};
   text-align: right;
 `;
 
-export const Paste = styled.button`
-  height: auto;
-  padding: 12px 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 15px;
-  margin-top: -3px;
-  border-radius: 4px;
-  background: ${(props) => props.theme.input.input_background};
-  border: 1px solid ${(props) => props.theme.input.input_border};
-  color: ${(props) => props.theme.type.secondary};
-  outline: none;
-  transition: 500ms;
-
-  &:hover {
-    color: ${(props) => props.theme.type.primary};
-    cursor: pointer;
-    transition: 500ms;
-    border: 1px solid #5c5f63;
-  }
-
-  ${media.mobile`
-    display:  none;
-    `}
-`;
-
 export const Container = styled.div`
-  width: auto;
-  height: auto;
   display: flex;
   flex-direction: column;
   padding-bottom: 12px;
@@ -50,7 +20,7 @@ export const Field = styled.textarea.attrs((props) => ({
 }))`
   display: flex;
   align-items: flex-start;
-  border: 1px solid #4a4d52;
+  border: 1px solid ${(props) => props.theme.input.input_border};
   border-bottom: none;
   border-radius: 4px;
   padding: 16px;
@@ -66,21 +36,19 @@ export const Field = styled.textarea.attrs((props) => ({
   color: ${(props) => props.theme.input.input_value};
 
   &:focus {
-    border: 1px solid #5c5f63;
+    border: 1px solid ${(props) => props.theme.input.input_border_focus};
     transition: 500ms;
   }
 
   &::placeholder {
     font-family: Inter-Regular;
     font-size: 16px;
-    color: #8a8d90;
+    color: ${(props) => props.theme.type.secondary};
     line-height: 26px;
   }
 `;
 
 export const Labels = styled.div`
-  height: auto;
-  width: auto;
   display: flex;
   justify-content: space-between;
   margin-bottom: 4px;
