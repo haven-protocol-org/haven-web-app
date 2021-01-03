b17a84764964198d2270db22524f5ee92663b1a7
Pierre Lafitte <pierrelafitte@protonmail.com>
2021-01-03 00:16:42 -0400

    Add additional copy to the modal

--- client/src/platforms/desktop/components/modals/rescanBCModal.tsx
+++ client/src/platforms/desktop/components/modals/rescanBCModal.tsx
@@ -4,22 +4,29 @@ import { HavenAppState } from "platforms/desktop/reducers/index.js";
 import { connect } from "react-redux";
 import { hideModal } from "shared/actions/modal";
 import LoginTutorial from "../../../../shared/components/tutorial/login/index.js";
+import { Label } from "../../../../assets/styles/type.js";
 
 class RescanBCM extends React.Component<any, any> {
-
   render() {
     return (
       <Modal
         title="Rescanning Wallet"
-        description={"We are rescanning from your first incoming TX at height " + this.props.restoreHeight }
+        description={
+          "We are rescanning from your first incoming TX at height " +
+          this.props.restoreHeight
+        }
         leftButton="..."
         rightButton="..."
         disabled={true}
         isLoading={true}
-        onConfirm={() => 1 }
+        onConfirm={() => 1}
         onCancel={() => 1}
       >
-
+        <Label>
+          If you have a trasnaction that's stuck, then you can click the Refresh
+          button below to rescan the blockchain. Once the rescan has started the
+          vault will become unresponsive.
+        </Label>
       </Modal>
     );
   }
@@ -30,9 +37,7 @@ class RescanBCM extends React.Component<any, any> {
 }
 
 const mapStateToProps = (state: HavenAppState) => ({
-
   restoreHeight: state.walletSession.restoreHeight,
-
 });
 
 export const RescanBCMModal = connect(mapStateToProps, { hideModal })(
