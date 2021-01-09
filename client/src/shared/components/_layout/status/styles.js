553c5c9cb08c2ef2143c8aa6fd5eaca9c2b3b3c5
Marty <juhu>
2021-01-09 19:40:59 +0100

    allow multiple status messages

--- client/src/shared/components/_layout/status/styles.js
+++ client/src/shared/components/_layout/status/styles.js
@@ -12,6 +12,8 @@ const load = keyframes`
 }
 `;
 
+
+
 export const Container = styled.div`
   height: auto;
   width: 100%;
@@ -19,7 +21,7 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 1000;
-  bottom: 0;
+  bottom: ${(props) => props.count * 50 + "px"};
   position: absolute;
   animation: ${load} 0.5s alternate;
 
