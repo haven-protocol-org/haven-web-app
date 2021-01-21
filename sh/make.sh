619a508ff12d7478f99a9de3300dd661099a911d
Marty <juhu>
2021-01-21 09:18:54 +0100

    Update make.sh

--- sh/make.sh
+++ sh/make.sh
@@ -6,27 +6,26 @@ NET_TYPE_ID=$1
 
 if [ -z "${NET_TYPE_ID}" ]; then
     PS3="please select net type: "
-    select opt in "mainnet" "testnet" "stagenet"
-
-        do
-            case $opt in
-                "mainnet")
-                    NET_TYPE_ID=0
-                    break
-                    ;;
-                "testnet")
-                    NET_TYPE_ID=1
-                    break
-                    ;;
-                "stagenet")
+    select opt in "mainnet" "testnet" "stagenet" do
+        
+        case $opt in
+            "mainnet")
+                NET_TYPE_ID=0
+                break
+                ;;
+            "testnet")
+                NET_TYPE_ID=1
+                break
+                ;;
+            "stagenet")
                 NET_TYPE_ID=2
-                    break
-                    ;;
-                    *)
-                    echo "Invalid option"
-                    ;;
-            esac
-        done
+                break
+                ;;
+                *)
+                echo "Invalid option"
+                ;;
+        esac
+    done
 fi
 
 export HAVEN_DESKTOP_DEVELOPMENT=false
