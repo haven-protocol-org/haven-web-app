53bf7c69a6489c63ee6b3a8ea23c68fe517d73cd
Pierre Lafitte <pierrelafitte@protonmail.com>
2021-03-31 09:07:26 -0400

    Clean up constants

--- client/src/constants/assets.ts
+++ client/src/constants/assets.ts
@@ -3,91 +3,34 @@ import { Ticker } from "shared/reducers/types";
 // id matches Ticker enum
 export const AssetList = [
   {
-    id:Ticker.xUSD,
+    id: Ticker.xUSD,
     token: "United States Dollar",
     ticker: "USD",
     symbol: "$",
   },
   {
-    id:Ticker.xEUR,
+    id: Ticker.xEUR,
     token: "Euro",
     ticker: "EUR",
     symbol: "€",
   },
   {
-    id:Ticker.xCNY,
+    id: Ticker.xCNY,
     token: "Chinese Yuan",
     ticker: "CNY",
     symbol: "¥",
   },
   {
-    id:Ticker.XAU,
+    id: Ticker.XAU,
     token: "Gold",
     ticker: "GOLD",
     symbol: "$",
   },
   {
-    id:Ticker.XAG,
+    id: Ticker.XAG,
     token: "Silver",
     ticker: "SILV",
     symbol: "$",
   },
 ];
 
-// {
-//   token: "Bitcoin",
-//   ticker: "BTC",
-//   price: "$1.00",
-//   change: "~ 0.00",
-//   symbol: "$",
-// },
-// {
-//   token: "Swiss Franc",
-//   ticker: "CHF",
-//   price: "$1.00",
-//   change: "~ 0.00",
-//   symbol: "₣",
-// },
-// {
-//   token: "Canadian Dollar",
-//   ticker: "CAD",
-//   price: "$1.00",
-//   change: "~ 0.00",
-//   symbol: "$",
-// },
-//
-// {
-//   token: "Australian Dollar",
-//   ticker: "AUD",
-//   price: "$1.00",
-//   change: "~ 0.00",
-//   symbol: "$",
-// },
-// {
-//   token: "New Zealand Dollar",
-//   ticker: "NZD",
-//   price: "$1.00",
-//   change: "~ 0.00",
-//   symbol: "$",
-// },
-// {
-//   token: "Norwegian Krone",
-//   ticker: "NOK",
-//   price: "$1.00",
-//   change: "~ 0.00",
-//   symbol: "kr",
-// },
-// {
-//   token: "Japanese Yen",
-//   ticker: "JPY",
-//   price: "$1.00",
-//   change: "~ 0.00",
-//   symbol: "¥",
-// },
-// {
-//   token: "British Pound",
-//   ticker: "GBP",
-//   price: "$1.00",
-//   change: "~ 0.00",
-//   symbol: "£",
-// },
