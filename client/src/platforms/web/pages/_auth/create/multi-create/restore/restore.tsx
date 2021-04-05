df65858c33df1c3156b07cd356b5e0ab4a475c69
Pierre Lafitte <pierrelafitte@protonmail.com>
2021-04-05 08:16:05 -0400

    Remove booleans that were causing input errors

--- client/src/platforms/web/pages/_auth/create/multi-create/restore/restore.tsx
+++ client/src/platforms/web/pages/_auth/create/multi-create/restore/restore.tsx
@@ -282,8 +282,8 @@ class RestoreWeb extends Component<RestoreProps, RestoreState> {
               onChange={this.handleChange}
               onClick={this.showPassword}
               error={error}
-              width={false}
               readOnly={false}
+              width
             />
             <Information>
               Re-enter the password you used to create this vault file. If you
