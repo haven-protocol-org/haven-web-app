debb28c8f1d4eadca021df4d9b255a5b4d04c6b2
Marty <51510876+MartyHav@users.noreply.github.com>
2021-04-09 09:31:15 +0200

    versionize onboarding

--- client/src/platforms/web/routes/public/index.js
+++ client/src/platforms/web/routes/public/index.js
@@ -9,12 +9,14 @@ import { LoginWeb } from "../../pages/_auth/login/container";
 import { showModal } from "shared/actions/modal";
 import { MODAL_TYPE } from "shared/reducers/modal";
 import { isWeb } from "constants/env";
+import { logM } from "utility/utility";
 
 class PublicRoutes extends Component {
   componentDidMount() {
-    if (localStorage.getItem("onboard") === null && isWeb() ) {
+
+    if (localStorage.getItem("onboard") !== "1" && isWeb() ) {
       this.props.showModal(MODAL_TYPE.LoginOnboarding);
-      localStorage.setItem("onboard", true);
+      localStorage.setItem("onboard", 1);
     }
   }
 
