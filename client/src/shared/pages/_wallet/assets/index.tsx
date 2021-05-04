7c7e3b46be760c616d34bf9c1249a285ac292def
Marty <51510876+MartyHav@users.noreply.github.com>
2021-05-04 15:56:04 +0200

    add decimals for btc

--- client/src/shared/pages/_wallet/assets/index.tsx
+++ client/src/shared/pages/_wallet/assets/index.tsx
@@ -63,7 +63,7 @@ class AssetsPage extends Component<AssetsProps, any> {
 
       const xTicker = id;
 
-      const numDecimals = (xTicker === Ticker.XAG || xTicker === Ticker.XAU) ? 4 : 2;
+      const numDecimals = (xTicker === Ticker.XAG || xTicker === Ticker.XAU || xTicker === Ticker.xBTC) ? 4 : 2;
 
       const unlockedBalance = convertBalanceToMoney(
         this.props.balances[xTicker].unlockedBalance, numDecimals
