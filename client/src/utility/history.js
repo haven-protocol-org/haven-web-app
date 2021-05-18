6245df2aec05b03a6bafc8a1f6ff2631f690be79
Marty <51510876+MartyHav@users.noreply.github.com>
2021-05-18 16:46:41 +0200

    serve app from any subfolder

--- client/src/utility/history.js
+++ client/src/utility/history.js
@@ -1,3 +1,3 @@
 import { createBrowserHistory } from "history";
 
-export const history = createBrowserHistory();
+export const history = createBrowserHistory({basename:window.location.pathname.replace(/(\/[^/]+)$/, "")});
