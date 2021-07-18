b27cc2bb87be6e6f1d3f49f0737393c8a5c3bbdd
Marty <51510876+MartyHav@users.noreply.github.com>
2021-07-18 20:29:41 +0200

    Update refresh.ts

--- client/src/shared/actions/refresh.ts
+++ client/src/shared/actions/refresh.ts
@@ -1,9 +1,11 @@
 import { isDesktop } from "constants/env";
 import { getLocalNodeState } from "platforms/desktop/actions/localNode";
 import { HavenAppState } from "platforms/desktop/reducers";
+import { getNodeForWallet } from "platforms/web/actions";
 import { walletProxy } from "shared/core/proxy";
 import { MODAL_TYPE } from "shared/reducers/modal";
 import { getHeightOfFirstIncomingTx, hasNoTxsEntries } from "shared/reducers/xTransferList";
+import { logM } from "utility/utility";
 import { getAppConnectionState, getWalletConnectionState } from "./connection";
 import { hideModal, showModal } from "./modal";
 import { getAllTransfers } from "./transferHistory";
@@ -24,6 +26,37 @@ export const refresh = () => {
 }
 
 
+export const syncRollback = () => {
+
+    return async(dispatch: any, getState:() =>  HavenAppState) => {
+        
+       
+         //@ts-ignore
+     //   await walletProxy.setDaemonConnection();
+        await walletProxy.stopSyncing(); 
+        let rollbackSyncHeight = 886570;
+        logM("rollback");
+        let currentSyncHeight = await walletProxy.getSyncHeight();
+        logM("currentSyncHeight : "+ currentSyncHeight)
+        // set temporary sync height to rollback height
+        await walletProxy.setSyncHeight(rollbackSyncHeight);
+        dispatch(showModal(MODAL_TYPE.RescanBC));
+        //trigger rescan
+        logM("start rescan");
+       // walletProxy.setDaemonConnection(getNodeForWallet(getState)!);
+        await walletProxy.rescanBlockchain();
+        logM("rescan finished");
+        // set back to correct refresh height
+        await walletProxy.setSyncHeight(currentSyncHeight);
+        dispatch(saveWallet());
+        dispatch(hideModal());
+        // start normal sync again
+        walletProxy.syncWallet();
+    }
+}
+
+
+
 
 export const syncFromFirstIncomingTx = () => {
 
@@ -34,6 +67,8 @@ export const syncFromFirstIncomingTx = () => {
             await dispatch(getAllTransfers());
         }
 
+        let currentSyncHeight = await walletProxy.getSyncHeight();
+
         let newSyncHeight = getHeightOfFirstIncomingTx(getState());
 
         dispatch({type: SET_RESTORE_HEIGHT, payload: newSyncHeight});
@@ -42,7 +77,7 @@ export const syncFromFirstIncomingTx = () => {
         if (newSyncHeight !== -1) {
 
             // give 5 blocks tolerance 
-            newSyncHeight = Math.max(newSyncHeight -5, 0);
+            newSyncHeight = Math.max(newSyncHeight -5, currentSyncHeight, 0);
             await walletProxy.setSyncHeight(newSyncHeight);
         }
         dispatch(showModal(MODAL_TYPE.RescanBC));
