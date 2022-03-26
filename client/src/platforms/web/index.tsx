78c17d07abf63f8146fe97b4029536bf98066deb
Lance Lumix <lance.lumix@gmail.com>
2022-03-26 18:34:49 -0400

    Added Redux Dev Tool for web dev

--- client/src/platforms/web/index.tsx
+++ client/src/platforms/web/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { GlobalStyle } from "../../globalStyle";
 import React from "react";
-import { applyMiddleware, createStore, Store } from "redux";
+import { applyMiddleware, createStore, Store, compose } from "redux";
 import reduxThunk from "redux-thunk";
 import reducers from "./reducers";
 import { logger } from "../../vendor/clipboard/dev-helper";
@@ -18,13 +18,15 @@ export const startWebApp = () => {
 };
 
 export const startWebAppInDevMode = () => {
-  const createStoreWithMiddleware = applyMiddleware(
-    reduxThunk,
-    logger
-  )(createStore);
-  store = createStoreWithMiddleware(reducers);
+  store = createStore(
+    reducers,
+    {},
+    compose(
+      applyMiddleware(reduxThunk, logger),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
   addStoreWatchers(store);
-
   render();
 };
 
