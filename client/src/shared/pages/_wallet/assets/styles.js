4a0fcfe74d081b5b6ec526be60c127403bdbff42
borgvin <borgvin@havenprotocol.org>
2024-10-15 13:12:05 +0100

    add protocol stats to assets page

--- client/src/shared/pages/_wallet/assets/styles.js
+++ client/src/shared/pages/_wallet/assets/styles.js
@@ -6,10 +6,10 @@ export const Row = styled.div`
   display: grid;
   grid-column: 1 / 3;
   grid-gap: 20px;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   height: auto;
 
   ${media.tablet`
-    grid-template-columns: 1fr;
+    grid-template-columns: 1fr 1fr;
   `}
 `;
